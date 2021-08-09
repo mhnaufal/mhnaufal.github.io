9cf31d6645d598e839041607c5e4a67bf4bfb136
mhnaufal <aal.pirates16@gmail.com>
2021-08-09 09:35:50 +0700

    Edit background, piechart, CV, and some text

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -58,7 +58,7 @@
                     animate: 2000,
                     lineCap:'butt',
                     scaleColor: false,
-                    barColor: '#FF5252',
+                    barColor: '#233287',
                     trackColor: 'transparent',
                     lineWidth: 10
                 });
